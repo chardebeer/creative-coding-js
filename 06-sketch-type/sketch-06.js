89a3e0b2ce7f9731c206145bbbfe2752615b4f00
Charlize de Beer <chardebeer94@gmail.com>
2021-12-12 20:21:33 +0200

    TODO: Get better image with colors

--- 06-sketch-type/sketch-06.js
+++ 06-sketch-type/sketch-06.js
@@ -1,65 +1,115 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const Colour = require('canvas-sketch-util/color');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-let manager;
+let manager, image;
 
-let text = 'A';
+let text = 'C';
 let fontSize = 1200;
-let fontFamily =  'serif';
+let fontFamily = 'serif';
 
-const sketch = () => {
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width  / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width  = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
+
+    // Draw the image here
+    typeContext.drawImage(image, 0, 0, cols, rows);
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
 
     context.fillStyle = 'black';
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = 'top';
-   // context.textAlign = 'center';
+    context.fillRect(0, 0, width, height);
+
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
 
 
-    const metrics = context.measureText(text);
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
 
-    const mx = metrics.actualBoundingBoxLeft * -1;
-    const my = metrics.actualBoundingBoxAscent * -1;
-    const  mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
-    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+      const x = col * cell + random.range(-cell, cell) * 0.5;
+      const y = row * cell + random.range(-cell, cell) * 0.5;
 
-    const  x = (width - mw) * 0.5 - mx;
-    const  y = (height - mh) * 0.5 - my;
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      const glyph = getGlyph(r);
+
+      context.font = `${cell * 2}px ${fontFamily}`;
+      if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
 
 
 
-    context.save();
-    context.translate(x, y);
+      context.fillStyle = 'white';
 
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
+      context.save();
+      context.translate(x, y);
+      context.translate(cell * 0.5, cell * 0.5);
 
-    context.fillText(text, 0, 0);
-    context.restore();
+      // context.fillRect(0, 0, cell, cell);
+
+      context.fillText(glyph, 0, 0);
+
+      context.restore();
+    }
+
+    // Comment this out to hide the small image from the top-left
+    context.drawImage(typeCanvas, 0, 0);
   };
 };
 
+const getGlyph = (v) => {
+  if (v < 50) return '';
+  if (v < 100) return '-';
+  if (v < 150) return '—';
+  if (v < 200) return '=';
+
+  const glyphs = 'Торогулова'.split('');
+
+  return random.pick(glyphs);
+};
+
+/*
 const onKeyUp = (e) => {
-  text = e.key;
+  text = e.key.toUpperCase();
   manager.render();
 };
 
+document.addEventListener('keyup', onKeyUp);
+*/
 
-document.addEventListener('keyup', onKeyUp)
+const loadMeSomeImage = (url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject();
+    img.src = url;
+  });
+};
 
-
-const start = async  () =>{
-   manager = await  canvasSketch(sketch, settings);
+const start = async () => {
+  const url = './The_Moon.png';
+  image = await loadMeSomeImage(url);
+  manager = await canvasSketch(sketch, settings);
 };
 
 start();
-
-
-
-
