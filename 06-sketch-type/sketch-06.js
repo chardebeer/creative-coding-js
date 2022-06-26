8b3232a22f8ce8286bb6405c8f489476277db2ee
Charlize de Beer <char@DESKTOP-AL5PEUK.localdomain>
2022-06-26 17:23:48 +0200

    glyphs

--- 06-sketch-type/sketch-06.js
+++ 06-sketch-type/sketch-06.js
@@ -1,119 +1,102 @@
 
-
-
 const canvasSketch = require('canvas-sketch');
-const random = require ('canvas-sketch-util/random');
-const color = require ('canvas-sketch-util/color');
+const random = require('canvas-sketch-util/random');
+const Colour = require('canvas-sketch-util/color');
 
 const settings = {
-  dimensions: [ 1080, 1080 ],
-  //animate: true
+  dimensions: [ 1080, 1080 ]
 };
 
-//declaring manager outside of the function to be used universal
 let manager, image;
 
-/*moved text variable outside the function of const sketch,
-which will allow changes*/
-let text ='A';
+let text = 'C';
 let fontSize = 1200;
-let fontFamily = 'helvetica';
+let fontFamily = 'serif';
 
 const typeCanvas = document.createElement('canvas');
 const typeContext = typeCanvas.getContext('2d');
 
-   const randomColor = () => {
-      let letters = '01ABCDEF';
-      let color = '#';
-
-      for(let i = 0; i < 6 ; i++){
-        color += letters[Math.floor(Math.random() * 8)];
-      }
-
-      return color;
-    };
-
-
-//make context, width, height data available for grid properties of typeCanvas to read
-const sketch = ({context, width, height}) => {
-
-  const cell = 18
-
-  const cols = Math.floor (width / cell);
-  const rows = Math.floor (height / cell);
+const sketch = ({ context, width, height }) => {
+  const cell = 8;
+  const cols = Math.floor(width  / cell);
+  const rows = Math.floor(height / cell);
   const numCells = cols * rows;
 
-  typeCanvas.width = cols;
+  typeCanvas.width  = cols;
   typeCanvas.height = rows;
 
-  return ({context, width, height }) => {
-    // color bg 
-    typeContext.fillStyle = '#dac4c7';
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = 'black';
     typeContext.fillRect(0, 0, cols, rows);
 
-    fontSize = cols;
-
-
-    //critical to display image on screen
+    // Draw the image here
     typeContext.drawImage(image, 0, 0, cols, rows);
-
-    typeContext.fillStyle = randomColor();
-
-    //typeContext.font = fontSize + 'px' + fontFamily;
-    typeContext.font = `$(fontSize)px $(fontFamily)`;
-    typeContext.textBaseline = 'middle';
-    typeContext.textAlign = 'center';
-
-    const metrics = typeContext.measureText(text);
-    const mx = metrics.actualBoundingBoxLeft * -1;
-    const my = metrics.actualBoundingBoxAscent * -1;
-    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
-    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
-
-    const tx = (cols - mw) * 0.5 - mx;
-    const ty = (rows - mh) * 0.5 - my;
-
-    typeContext.save();
-    typeContext.translate(tx,ty);
 
     const typeData = typeContext.getImageData(0, 0, cols, rows).data;
 
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width  , height);
+
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
 
 
     for (let i = 0; i < numCells; i++) {
       const col = i % cols;
-      const row = Math.floor (i / cols);
+      const row = Math.floor(i / cols);
 
-
-      const x = col * cell;
-      const y = row * cell;
+      const x = col * cell + random.range(-cell, cell) * 0.5;
+      const y = row * cell + random.range(-cell, cell) * 0.5;
 
       const r = typeData[i * 4 + 0];
       const g = typeData[i * 4 + 1];
       const b = typeData[i * 4 + 2];
       const a = typeData[i * 4 + 3];
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      const glyph = getGlyph(r);
+
+      context.font = `${cell / 2}px ${fontFamily}`;
+      if (Math.random() < 0.21) context.font = `${cell % y * 3.6}px ${fontFamily}`;
+
+
+
+      context.fillStyle = 'white';
 
       context.save();
       context.translate(x, y);
       context.translate(cell * 0.5, cell * 0.5);
 
-      //circle drawing
-      context.beginPath();
-      context.arc(0, 0, cell * 0.4, 0.4, Math.PI * 2);
- //context.fillRect(0, 0, x, y);
+      // context.fillRect(0, 0, cell, cell);
 
-
-
-      context.fill();
-
+      context.fillText(glyph, 0, 0);
 
       context.restore();
     }
-  };
 
+    // Comment this out to hide the small image from the top-left
+   // context.drawImage(typeCanvas, 0, 0);
+  };
 };
+
+const getGlyph = (v) => {
+  if (v < 1) return ' ';
+  if (v < 50) return '🌼';
+  if (v < 100) return '🤍';
+  if (v < 200) return '💀';
+
+  const glyphs = '            '.split('    ');
+
+  return random.pick(glyphs);
+};
+
+
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase();
+  manager.render();
+};
+
+document.addEventListener('keyup', onKeyUp);
+
 
 const loadMeSomeImage = (url) => {
   return new Promise((resolve, reject) => {
@@ -121,18 +104,13 @@ const loadMeSomeImage = (url) => {
     img.onload = () => resolve(img);
     img.onerror = () => reject();
     img.src = url;
-
   });
 };
 
 const start = async () => {
-  const url = './soul.jpg'
+  const url = './soul2.jpg';
   image = await loadMeSomeImage(url);
   manager = await canvasSketch(sketch, settings);
-
 };
 
 start();
-
-
-
